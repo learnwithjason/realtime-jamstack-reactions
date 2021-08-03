ae89ca06586c2df58745a56c30433df5391d92ca
Jason Lengstorf <jason@lengstorf.com>
2021-08-03 11:34:36 -0700

    wip: COOOOOORS

--- netlify/functions/ably-auth-url.js
+++ netlify/functions/ably-auth-url.js
@@ -1,7 +1,22 @@
 const Ably = require('ably');
 const rest = new Ably.Rest({ key: process.env.ABLY_API_KEY });
 
-exports.handler = (_event, _context, callback) => {
+exports.handler = (event, _context, callback) => {
+  if (event.httpMethod === 'OPTIONS') {
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    };
+
+    // https://stackoverflow.com/questions/55769272/how-to-set-up-cors-in-netlify-serverless-function
+    return {
+      statusCode: 200,
+      headers,
+      body: 'CORS is alright with me',
+    };
+  }
+
   rest.auth.createTokenRequest({}, (err, tokenRequest) => {
     if (err) {
       callback({
